660320b49c5a159a976de4ca4f26f4c3d165dac3
jiahao.zhu <jiahao.zhu@juniontech.com>
2023-03-07 14:24:02 +0800

    form JSON

--- JQuery_version.js
+++ JQuery_version.js
@@ -3,17 +3,28 @@ $(document).ready(function(){
         //防止表单默认提交
         event.preventDefault();
 
+        //获取时间
+        var now = new Date().toLocaleString();
+
         //获取表单数据
-        var formData = $(this).serialize();
+        var formData = {
+            Name: $('#name').val(),
+            Email: $('#email').val(),
+            Content: $('#comment').val(),
+            ReleaseTime: now
+        };
         console.log(formData);
+        console.log(JSON.stringify(formData));
 
         //发送AJAX请求
         $.ajax({
-            url: 'http://localhost:30003/Message/Test',
-            type: 'GET',
+            url: 'http://localhost:30003/Message/Test2',
+            method: 'POST',
+            data: JSON.stringify(formData),
+            contentType: 'application/json',
+            dataType: 'json',
             success: function(response){
                 console.log(response);
-                console.log("1");
             }
         })
     })
